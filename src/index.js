1f3fe9f112d50b40ceb2077cb613cf42d4df9c07
chrnk-exe <ivan_kot2001@mail.ru>
2022-08-29 16:11:02 +0300

    corstest2

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(cors(corsOptions))
+
+app.use(cors({origin: "http://84.252.128.241", credentials: true}))
 app.use(express.json())
 
 app.use(express.static(path.resolve(__dirname, '../build')))
