a06ecf1f246dba9166423b5c81b7cac99df68732
chrnk-exe <ivan_kot2001@mail.ru>
2022-08-29 15:50:38 +0300

    corstest

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function randomInteger(min, max) {
 const app = express()
 const port = 5000
 
-app.use(cors())
+app.use(cors({origin: true}))
 app.use(express.json())
 
 app.use(express.static(path.resolve(__dirname, '../build')))
