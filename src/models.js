ae2a0ba2111319e5c1101590ffbf49a673d5afb8
chrnk-exe <ivan_kot2001@mail.ru>
2022-08-29 19:22:11 +0300

    models

--- src/models.js
+++ src/models.js
@@ -1,64 +1,67 @@
 const sequelize = require('./db')
 const { Model, DataTypes } = require('sequelize')
+const { User } = require('./models/user')
+const { Comment } = require('./models/comment')
+const { Article } = require('./models/article')
 
 const { STRING, INTEGER, TEXT, DATE, BOOLEAN } = DataTypes
 
-class Articles extends Model {}
-class Users extends Model {}
-class Comments extends Model {}
+class Articles extends Article{}
+class Users extends User {}
+class Comments extends Comment {}
 
-Articles.init({
-    id: {
-        type: INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-    },
-    title: TEXT,
-    body: TEXT,
-    date_of_creation: DATE,
-    creatorid: INTEGER,
-    comments: TEXT
-}, {
-    sequelize,
-    tableName: 'Articles',
-    modelName: 'Article',
-    timestamps: true
-})
+// Articles.init({
+//     id: {
+//         type: INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//         allowNull: false
+//     },
+//     title: TEXT,
+//     body: TEXT,
+//     date_of_creation: DATE,
+//     creatorid: INTEGER,
+//     comments: TEXT
+// }, {
+//     sequelize,
+//     tableName: 'Articles',
+//     modelName: 'Article',
+//     timestamps: true
+// })
 
-Users.init({
-    id: {
-        type: INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-    },
-    login: STRING(30),
-    userpassword: STRING(100),
-    isadmin: BOOLEAN
-}, {
-    sequelize,
-    tableName: 'Users',
-    modelName: 'User',
-    timestamps: true
-})
+// Users.init({
+//     id: {
+//         type: INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//         allowNull: false
+//     },
+//     login: STRING(30),
+//     userpassword: STRING(100),
+//     isadmin: BOOLEAN
+// }, {
+//     sequelize,
+//     tableName: 'Users',
+//     modelName: 'User',
+//     timestamps: true
+// })
 
-Comments.init({
-    id: {
-        type: INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-    },
-    userid: INTEGER,
-    body: TEXT,
-    articleid: INTEGER
-}, {
-    sequelize,
-    tableName: 'Comments',
-    modelName: 'Comment',
-    timestamps: true
-})
+// Comments.init({
+//     id: {
+//         type: INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//         allowNull: false
+//     },
+//     userid: INTEGER,
+//     body: TEXT,
+//     articleid: INTEGER
+// }, {
+//     sequelize,
+//     tableName: 'Comments',
+//     modelName: 'Comment',
+//     timestamps: true
+// })
 
 // sequelize.sync()
 
