586cfa129c28100685c84354e461b314473dd5ff
chrnk-exe <ivan_kot2001@mail.ru>
2022-09-09 01:04:21 +0300

    str

--- src/api.js
+++ src/api.js
@@ -74,7 +74,7 @@ router.post('/create', async (req, res) => {
   		_.merge(info, JSON.parse(date))
   		if(info.flag){
 			info.flag = undefined
-  			res.json({'flag':'Привет_настя!'})
+  			res.json({'flag':'Привет_солнышко! <3'})
   		} else {
   			const result = api.addArticle(info)
   			res.json({status:true, 'info': 'article created'})
